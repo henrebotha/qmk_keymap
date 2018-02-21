519a6da160890f457ec663a73dc1317681df62bc
Henré Botha <henrebotha@gmail.com>
2018-02-21 18:59:11 +0200

    Make build script more configurable

--- build.sh
+++ build.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
+KEYBOARD=ergodox_ez
+KEYMAP=henrebotha
+QMK_DIR=qmk_firmware
 
 # 1. Copy my files into their appropriate locations inside QMK
-mkdir -p qmk_firmware/keyboards/ergodox_ez/keymaps/henrebotha
-cp src/keymaps/henrebotha/config.h qmk_firmware/keyboards/ergodox_ez/keymaps/henrebotha/
-cp src/keymaps/henrebotha/keymap.c qmk_firmware/keyboards/ergodox_ez/keymaps/henrebotha/
-cp src/keymaps/henrebotha/rules.mk qmk_firmware/keyboards/ergodox_ez/keymaps/henrebotha/
-cp src/keymaps/overrides/config.h qmk_firmware/keyboards/ergodox_ez/
+mkdir -p $QMK_DIR/keyboards/$KEYBOARD/keymaps/$KEYMAP
+cp -r src/keymaps/$KEYMAP $QMK_DIR/keyboards/$KEYBOARD/keymaps/
+cp -r src/keymaps/overrides/ $QMK_DIR/keyboards/$KEYBOARD
 
-# 2. Run string replacement to tweak EZ defaults (alternative to copying overrides dir)
+# 2. Run string replacement to tweak EZ defaults (this is an alternative to
+# copying the overrides dir)
 
 # 3. Make clean
-cd qmk_firmware
-make ergodox_ez:henrebotha:clean
+cd $QMK_DIR
+make $KEYBOARD:$KEYMAP:clean
 
 # 4. Make teensy build
-make ergodox_ez:henrebotha
+make $KEYBOARD:$KEYMAP
 
 # 5. Clean up
 cp -r .build ../.build
