8c3ca567d54869a337671d850b43263de0ce8a98
Henré Botha <henrebotha@gmail.com>
2019-01-16 10:38:50 +0100

    Fix mod-tap behaviour

--- src/keymaps/henrebotha/config.h
+++ src/keymaps/henrebotha/config.h
@@ -1,2 +1,8 @@
 #define LEADER_TIMEOUT 2000
 #define ONESHOT_TAP_TOGGLE 3
+// Affects behaviour of CTL_T(ESC) down, T down, CTL_T(ESC) up, T up
+// i.e. releasing the mod-tap key before releasing the other key.
+//
+// With I_M_T_I: emits Esc, T
+// Without:      emits Ctrl-T
+#undef IGNORE_MOD_TAP_INTERRUPT
