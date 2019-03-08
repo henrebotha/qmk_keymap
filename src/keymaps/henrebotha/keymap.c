525eda42149f08b9cc48e6d5009d9fb2f09db318
Henré Botha <henrebotha@gmail.com>
2019-03-08 17:20:47 +0100

    Remove unused key

--- src/keymaps/henrebotha/keymap.c
+++ src/keymaps/henrebotha/keymap.c
@@ -32,11 +32,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // home layer
   [LAYER_HOME] = LAYOUT_ergodox_pretty(
-    KC_GRAVE,         KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   TG(1),      RESET,    KC_6,    KC_7,    KC_8,      KC_9,     KC_0,     KC_BSPACE,
-    KC_TAB,           KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   LCTL(KC_B), _______,  KC_Y,    KC_U,    KC_I,      KC_O,     KC_P,     KC_BSLASH,
-    CTL_T(KC_ESCAPE), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,       KC_J,     KC_K,    KC_L,    KC_SCOLON, KC_QUOTE,
-    KC_LGUI,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_MINUS,   KC_EQUAL, KC_N,    KC_M,    KC_COMMA,  KC_DOT,   KC_SLASH, KC_RGUI,
-    KC_HYPR,          KC_TASK, KC_LEAD, OSL(3), OSL(2), OSL(2), OSL(3),     KC_LEAD,  _______, KC_HYPR,
+    KC_GRAVE,         KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   TG(1),    RESET,    KC_6,    KC_7,    KC_8,      KC_9,     KC_0,     KC_BSPACE,
+    KC_TAB,           KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   _______,  _______,  KC_Y,    KC_U,    KC_I,      KC_O,     KC_P,     KC_BSLASH,
+    CTL_T(KC_ESCAPE), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,     KC_J,     KC_K,    KC_L,    KC_SCOLON, KC_QUOTE,
+    KC_LGUI,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_MINUS, KC_EQUAL, KC_N,    KC_M,    KC_COMMA,  KC_DOT,   KC_SLASH, KC_RGUI,
+    KC_HYPR,          KC_TASK, KC_LEAD, OSL(3), OSL(2), OSL(2), OSL(3),   KC_LEAD,  _______, KC_HYPR,
 
     KC_MUTE,   KC_VOLU,  KC_MPRV, KC_MNXT,
     KC_VOLD,   KC_MPLY,
