f2feb7a93389c8c967fd29d7ca599665ac54fb8d
Brian Pruitt-Goddard <bpruitt-goddard@github>
2022-12-23 13:01:35 -0800

    feat: create initial build of default keymap
    update build file to use docker and provide baseline keymap

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Purpose
 Currently, QMK [stores all keymaps in the same repository](https://github.com/qmk/qmk_firmware/issues/1120) as the firmware code. This makes it difficult to work with a keymap as the entire QMK source code needs to be stored along with the keymap in order to get the latest updates. This repository tries to make this process slightly easier by keeping the QMK firmware as a git submodule and keeping the custom keymaps at the root of the repository.
-The build script then handles the task of moving the keymaps into the correct location in the QMK firmware and building the firmware hex file.  
+The build script then handles the task of moving the keymaps into the correct location in the QMK firmware and building the firmware hex file.
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-KEYBOARD=ergodox_ez
-KEYMAP=henrebotha
+KEYBOARD=crkbd/rev1
+KEYMAP=bpruitt-goddard
 QMK_DIR=qmk_firmware
 
 set -e
@@ -10,20 +10,14 @@ rm -rf .build
 
 # 1. Copy my files into their appropriate locations inside QMK
 mkdir -p $QMK_DIR/keyboards/$KEYBOARD/keymaps/$KEYMAP
-cp -r src/keymaps/$KEYMAP $QMK_DIR/keyboards/$KEYBOARD/keymaps/
-# cp -r src/keymaps/overrides/ $QMK_DIR/keyboards/$KEYBOARD
+cp -r keymaps/$KEYMAP $QMK_DIR/keyboards/$KEYBOARD/keymaps/
 
-# 2. Run string replacement to tweak EZ defaults (this is an alternative to
-# copying the overrides dir)
-
-# 3. Clean
+# 2. Compile
 cd $QMK_DIR
-qmk clean --all
+util/docker_build.sh $KEYBOARD:$KEYMAP
 
-# 4. Compile
-qmk compile -kb ergodox_ez -km henrebotha
-
-# 5. Clean up
-cp -r .build ../.build
+# 3. Clean up
+mkdir ../.build
+cp -r .build/*.hex ../.build
 git clean -f -d
 git checkout -- .
