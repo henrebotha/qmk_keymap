7b8de4815165efaa10da48d001c2752a3a311c30
Brian Pruitt-Goddard <bpruitt-goddard@github>
2022-12-27 15:59:14 -0800

    feat: add home row mods

--- keymaps/bpruitt-goddard/config.h
+++ keymaps/bpruitt-goddard/config.h
@@ -28,8 +28,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-//#define TAPPING_FORCE_HOLD
-//#define TAPPING_TERM 100
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+#define PERMISSIVE_HOLD
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
